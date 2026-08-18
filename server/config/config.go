005259e383d458b237fafe0e204e28e9899f3a20
Min Ming Di <bjdmmdi@cn.ibm.com>
2026-08-19 00:33:35 +0800

    完成配置文件的读取

--- server/config/config.go
+++ server/config/config.go
@@ -9,12 +9,42 @@ import (
 
 var Config *CfgDict
 
+func init() {
+	err := ReadCfgFromFile("config.yaml")
+	if err != nil {
+		panic(fmt.Sprintf("failed to read config file: %v", err))
+	}
+}
+
 type CfgDict struct {
 	Storage struct {
 		File struct {
 			Path string `yaml:"path"`
 		} `yaml:"file"`
 	} `yaml:"storage"`
+	Cache struct {
+		Type     string `yaml:"type"`
+		Addr     string `yaml:"addr"`
+		Password string `yaml:"password"`
+		DBIndex  int    `yaml:"db_index"`
+	} `yaml:"cache"`
+	Model struct {
+		Embedding struct {
+			APIURL    string `yaml:"api_url"`
+			ModelName string `yaml:"model_name"`
+			APIKey    string `yaml:"api_key"`
+		} `yaml:"embedding"`
+		Reranking struct {
+			APIURL    string `yaml:"api_url"`
+			ModelName string `yaml:"model_name"`
+			APIKey    string `yaml:"api_key"`
+		} `yaml:"reranking"`
+		LLM struct {
+			APIURL    string `yaml:"api_url"`
+			ModelName string `yaml:"model_name"`
+			APIKey    string `yaml:"api_key"`
+		} `yaml:"llm"`
+	} `yaml:"model"`
 }
 
 func ReadCfgFromFile(filepath string) error {
